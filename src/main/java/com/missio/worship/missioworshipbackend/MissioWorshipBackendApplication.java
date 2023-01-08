c02bd1486993649450c5177851a50e9d8a6170a8
Aarón J. Morales Botton <aaron@midokura.com>
2023-01-08 17:39:29 +0100

    First commit, initial code

---
+++ src/main/java/com/missio/worship/missioworshipbackend/MissioWorshipBackendApplication.java
@@ -0,0 +1,13 @@
+package com.missio.worship.missioworshipbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissioWorshipBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MissioWorshipBackendApplication.class, args);
+	}
+
+}
