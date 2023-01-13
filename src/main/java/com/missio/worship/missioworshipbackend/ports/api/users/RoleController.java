55082d8780bf903050acd8d71216e04d188b70ba
Aaron J. Morales Botton <aaron@mbotton.com>
2023-01-13 22:42:10 +0100

    RoleController

---
+++ src/main/java/com/missio/worship/missioworshipbackend/ports/api/users/RoleController.java
@@ -0,0 +1,8 @@
+package com.missio.worship.missioworshipbackend.ports.api.users;
+
+public interface RoleController {
+    /**
+     * Hay que crear un CRUD básico para crear y matar roles. Tambien una lista
+     * Con eso creamos roles y probamos de crear usuarios.
+     */
+}
