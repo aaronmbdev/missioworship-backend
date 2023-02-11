3509113c33b37a33dc193d74e8ed02c9fa34d1c5
Aarón J. Morales Botton <aaron@midokura.com>
2023-02-11 14:07:11 +0100

    fix::removing non-thread safe variable

--- src/main/java/com/missio/worship/missioworshipbackend/libs/utils/DateUtils.java
+++ src/main/java/com/missio/worship/missioworshipbackend/libs/utils/DateUtils.java
@@ -6,13 +6,12 @@ import java.util.Date;
 
 public class DateUtils {
     private static String PATTERN = "yyyy-MM-dd";
-    private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat(PATTERN);
 
     public static String parseFrom(final Date date) {
-        return simpleDateFormat.format(date);
+        return new SimpleDateFormat(PATTERN).format(date);
     }
 
     public static Date parseFrom(final String date) throws ParseException {
-        return simpleDateFormat.parse(date);
+        return new SimpleDateFormat(PATTERN).parse(date);
     }
 }
