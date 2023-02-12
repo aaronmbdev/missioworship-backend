5e83a12bac06d4e519e7d00b8035846d7cd49804
Aarón J. Morales Botton <aaron@midokura.com>
2023-02-12 16:51:55 +0100

    Updated end-to-end

--- e2e/run_e2e.sh
+++ e2e/run_e2e.sh
@@ -1,2 +1,3 @@
 newman run role_user.postman_collection.json -e local_environment.json
 newman run absences.postman_collection.json -e local_environment.json
+newman run songs.postman_collection.json -e local_environment.json
